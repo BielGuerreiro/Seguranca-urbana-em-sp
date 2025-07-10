a3855853eb72f37fec6232377c62f1902677c701
Gabriel Guerreiro <bielguerreiro2526@gmail.com>
2025-07-10 01:59:41 -0300

    fifth commit

--- script.js
+++ script.js
@@ -10,3 +10,43 @@ botao.addEventListener('mousemove', e => {
   inner.style.setProperty('--mouseX', `${x}px`);
   inner.style.setProperty('--mouseY', `${y}px`);
 });
+
+// efeito celular ____________________________________________________________
+
+document.addEventListener("DOMContentLoaded", () => {
+  const gps = document.getElementById("gps");
+
+  const observerGps = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        gps.classList.add("ligado");
+      }
+    });
+  }, { threshold: 0.4 });
+
+  if (gps) observerGps.observe(gps);
+
+  // efeito universal ________________________________________________________________
+  
+  const observerVisual = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visivel");
+        observerVisual.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.3 });
+
+  document.querySelectorAll(".efeito1").forEach((el) => {
+    observerVisual.observe(el);
+  });
+
+  document.querySelectorAll(".efeito2").forEach((el) => {
+    observerVisual.observe(el);
+  });
+
+  document.querySelectorAll(".efeito3").forEach((el) => {
+    observerVisual.observe(el);
+  });
+});
+
